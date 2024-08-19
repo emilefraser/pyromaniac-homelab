c3b790ae8551451aed8e46039f147bb99b730e0b
emilefraser <efraser25@gmail.com>
2024-08-20 01:28:16 +0200

    updated output environment

--- operations/terraform/outputs.tf
+++ operations/terraform/outputs.tf
@@ -3,23 +3,7 @@ output "environment_code" {
   value       = var.environment_code
 }
 
-output "region_code" {
-  description = "Region Code"
-  value       = var.region_code
-}
-
 output "product_storage_account_name" {
   description = "Product Storage Name"
   value       = module.storage.storage_account_name
-}
-
-output "storage_account_id" {
-  description = "Product Storage Name"
-  value       = module.storage.storage_account_id
-}
-
-output "product_storage_account_primary_access_key" {
-  description = "Product Storage AccessKey"
-  value       = module.storage.storage_account_primary_access_key
-  sensitive   = true
 }
